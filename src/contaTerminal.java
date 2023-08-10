a19cf1b655471bfedf6be14c788b658e0c2278d0
Neto <franciscomdlneto@gmail.com>
2023-08-09 21:31:18 -0300

    minha classe contaBanco

--- src/contaTerminal.java
+++ src/contaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*TODO: Conhecer e importar a classe scanner;
+        *       Exibir as mensagens para o nosso usuário;
+        *       Obter pelo scanner os valores digitados no terminal
+        *       Exibir a mensagem conta criada */
+        int contaNumero;
+        String contaAgencia;
+        String nomeCliente;
+        float saldo = 0;
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Seja bem vindo ao banco Java.");
+        System.out.println("Por favor, digite o número da Conta:");
+        contaNumero = leitor.nextInt();
+        System.out.println("Por favor, digite o número da Agência:");
+        contaAgencia = leitor.nextLine();
+        System.out.println("Por favor, digite o seu Nome:");
+        nomeCliente = leitor.nextLine();
+        System.out.println("");
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é ["+contaAgencia+"], conta["+contaNumero+"], e seu saldo ["+saldo+"] já está dispoível para saque.");
     }
 }
